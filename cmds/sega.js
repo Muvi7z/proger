d7f31f25d71d1760686dafc185b2907392c12e85
Muvi7z <ochir-zhamsuev@yandex.ru>
2020-05-16 20:46:58 +0800

    just commit

---
+++ cmds/sega.js
@@ -0,0 +1,22 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+let profile= require("../profile.json");
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  var filterFloat = function (value) {
+    if(/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
+      .test(value))
+      return Number(value);
+  return false;
+}
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('Число:'+getRandomInt(0,args[0]));
+    fs.writeFile('./profile.json',JSON.stringify(profile),(err)=>{
+        if(err) console.log(err);
+    })
+    
+};
+module.exports.help = {
+    name: "sega"
+};
