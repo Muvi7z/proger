36694a4bfaf8146b22bde7a505a65e9932c22776
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 14:19:03 +0800

    just

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
   // Get the channel via ID
   let channel = client.channels.get('622718588989734929');
   // Or via name (less persistent)
-  channel = client.channels.find('GJNJR', 'Music');
+  channel = client.channels.find('GJNJR');
 
   channel.join()
   .then(connection => console.log('Connected'))
