b401bc17b64d90bc7cc2f58ead4efd4f47cb51a3
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 14:49:35 +0800

    1

--- bot.js
+++ bot.js
@@ -25,15 +25,14 @@ bot.on('ready', () => {
       console.log(link);
   })
 });
-bot.on('ready', () => {
-  // Get the channel via ID
-  let channel = bot.channels.get('622718588989734929');
-
-
-  channel.join()
-  .then(connection => console.log('Connected'))
-  .catch(console.error);
-});
+client.setInterval(() => {
+  const guild = client.guilds.cache.get(622718588356132884);
+  const voiceChannel = guild.channels.get(622718588989734929);
+  voiceChannel.members.forEach(member => {
+      // Give users a coin
+      console.log("yes");
+  })
+}, 60000)
 bot.on('message', async message => {
   let messageArray = message.content.split(" ");
   let command = messageArray[0].toLowerCase();
