4af4a0ebf6628326877ed12dc4498c80558d123d
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 14:58:28 +0800

    ju1

--- cmds/tylop.js
+++ cmds/tylop.js
@@ -1,18 +1,9 @@
 const Discord = module.require("discord.js");
 const client = new Discord.Client();
-
+const sega = require('./sega.js');
 const fs = require("fs");
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  var filterFloat = function (value) {
-    if(/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/
-      .test(value))
-      return Number(value);
-  return false;
-}
 client.setInterval(() => {
-  const guild = client.guilds.cache.get(622718588356132884);
+  const guild = client.guilds.cache.get('622718588356132884');
   const voiceChannel = guild.channels.get(622718588989734929);
   voiceChannel.members.forEach(member => {
       // Give users a coin
@@ -21,7 +12,7 @@ client.setInterval(() => {
 }, 60000)
 module.exports.run = async (bot,message,args) => {
     message.channel.send(voiceChannel.members.size);
-    message.channel.send('Число:'+getRandomInt(1,args[0]));
+    message.channel.send('Число:'+sega.getRandomInt(1,args[0]));
 };
 client.login(process.env.BOT_TOKEN);
 module.exports.help = {
