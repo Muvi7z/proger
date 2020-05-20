08bd6a59142f07b6a713c40b3bf8b4fb83b96e9b
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 17:46:41 +0800

    1

--- cmds/sega.js
+++ cmds/sega.js
@@ -10,8 +10,18 @@ var filterFloat = function (value) {
   return false;
 }
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('Число:'+getRandomInt(1,args[0]));
-    message.guild.channels.get()
+    if(args[0]!=null)
+    {
+      message.channel.send('Случайное число:'+getRandomInt(1,args[0]));
+      message.guild.channels.get()
+    }
+    else{
+      const GuildMember = message.guild.members;
+      const channel = message.guild.channels.get('622718588989734929'); //replace voiceChannelID with your voice Channel ID
+      let toCheck = channel.members;
+      message.channel.send('Я выбираю тебя: ' + toCheck.random());
+    }
+
 };
 module.exports.help = {
     name: "sega"
