221296155a2307de2b6b276ec1fc1b252bc823dd
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 15:12:22 +0800

    1

--- cmds/tylop.js
+++ cmds/tylop.js
@@ -1,8 +1,8 @@
 const Discord = module.require("discord.js");
-const client = new Discord.Client();
 const sega = require('./sega.js');
 const fs = require("fs");
 client.setInterval(() => {
+  
   const guild = client.guilds.cache.get('622718588356132884');
   const voiceChannel = guild.channels.get(622718588989734929);
   voiceChannel.members.forEach(member => {
