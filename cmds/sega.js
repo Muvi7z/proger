f1381e197ab250846f6dc860499d3b417be1102f
Muvi7z <ochir-zhamsuev@mail.ru>
2020-05-20 19:11:35 +0800

    1

--- cmds/sega.js
+++ cmds/sega.js
@@ -17,10 +17,23 @@ module.exports.run = async (bot,message,args) => {
     }
     else{
       const GuildMember = message.guild.members;
-      const channel = message.guild.channels.get('622718588989734929'); //replace voiceChannelID with your voice Channel ID
-      let toCheck = channel.members;
-      message.channel.send('Я выбираю тебя: ' + toCheck.random());
+      // const channel = message.guild.channels.get('622718588989734929'); //replace voiceChannelID with your voice Channel ID
+      // let toCheck = channel.members;
+      // message.channel.send('Я выбираю тебя: ' + toCheck.random());
       /////////////////////////////////////////////////////////////////////////////////
+      const voiceChannels = message.guild.channels.filter(c => c.type === 'voice');
+      let count = 0;
+      for (const [id, voiceChannel] of voiceChannels)
+      {
+        if(voiceChannel.members.size>0)
+        {
+          channel = message.guild.channels.get(voiceChannel.id); //replace voiceChannelID with your voice Channel ID
+          toCheck = channel.members;
+          message.channel.send('Я выбираю тебя: ' + toCheck.random());
+        }
+        count += voiceChannel.members.size;
+      } 
+      console.log(count);
     }
 
 };
